6a44514aebd3ce9dba29a9489b90081f1f5431e3
colton <cloftus@princeton.edu>
2021-06-16 14:10:40 -0400

    Bug Fixes

--- csvOperations.py
+++ csvOperations.py
@@ -2,82 +2,31 @@ from csv import writer, DictWriter
 from enum import Enum, auto
 from posixpath import join
 import datetime, shutil
-import pandas as pd
 
 
 def createBackup(fileName):
     shutil.copy(fileName, join(fileName, datetime.date, ".backup"))
 
 
-# class ColumnIndex(Enum):
-#     # starts at 0 to have proper list indexing from 0
-#     A  = 0
-#     B  = auto()
-#     C  = auto()
-#     D  = auto()
-#     E  = auto()
-#     F  = auto()
-#     G  = auto()
-#     H  = auto()
-#     I  = auto()
-#     J  = auto()
-#     K  = auto()
-#     L  = auto()
-#     M  = auto()
-#     N  = auto()
-#     O  = auto()
-#     P  = auto()
-#     Q  = auto()
-#     R  = auto()
-#     S  = auto()
-#     T  = auto()
-#     U  = auto()
-#     V  = auto()
-#     W  = auto()
-#     X  = auto()
-#     Y  = auto()
-#     Z  = auto()
-
-# # double enum is used so it is clear both the number and the column index
-# # the usage of letters is for easy human-readability when comparing to the 
-# # spreadsheet
-# class ColumnName(Enum):
-#     farm                    = ColumnIndex.A
-#     crop                    = ColumnIndex.B
-#     time                    =  ColumnIndex.C
-#     julian                   = ColumnIndex.D
-#     date	                  = ColumnIndex.E
-#     device                 	 = ColumnIndex.F
-#     T_BASE                 	 = ColumnIndex.G
-#     GDD	 =                  ColumnIndex.H
-#     CGDD =                  ColumnIndex.I
-#     Oberserved_Growth_Stage = ColumnIndex.J
-#     Estimated_Growth_Stage = ColumnIndex.K
-#     cl =                   ColumnIndex.L
-#     NDVI =                  ColumnIndex.M
-#     ETc	 =                  ColumnIndex.N
-#     SWdw	                  = ColumnIndex.O
-#     maxT_C                   = ColumnIndex.P
-#     meanT_C                  = ColumnIndex.Q
-#     minT_C                  = ColumnIndex.R
-#     precip	                = ColumnIndex.S
-#     maxT_F                  = ColumnIndex.T
-#     meanT_F                 = ColumnIndex.U
-#     minT_F                   = ColumnIndex.V
-
-# class headers():
-    # fieldNames = [n for n in dir(ColumnName) if "__" not in n]
-
 def calcJulian(date):
-    # d = date.split("-")
-    # d = list(map(int, d))
-    # ts = pd.Timestamp(year = d[0], month=  d[1], day = d[2], 
-    #               tz = 'US/Eastern') 
-    fmt='%Y-%m-%d'
-    sdtdate = datetime.datetime.strptime(date, fmt)
-    sdtdate = sdtdate.timetuple()
-    jdate = sdtdate.tm_yday
+    try:
+        fmt='%Y-%m-%d'
+        sdtdate = datetime.datetime.strptime(date, fmt)
+        sdtdate = sdtdate.timetuple()
+        jdate = sdtdate.tm_yday
+    except:
+        return None
     return(jdate)
+
+def changeTimeFormat(date):
+    try:
+        s = date.split("-")
+        return (s[1] + "/" + s[2] + "/" + s[0])
+    except:
+        return None
+
+def hasData(input: list):
+    return
 
 
 def append_list_as_row(file_name, list_of_elem):
--- main.py
+++ main.py
@@ -1,17 +1,43 @@
 import csv, sys
+from shutil import Error
 from os import name
 from typing import Collection
 import arableAPI, plantFormulas, csvOperations
 from csv import writer
 from collections import defaultdict
 
+
 def main():
+
+    farms = {
+                "CG" : "Cherry Grove",
+                "AA": " Abe's Acres",
+                "OO": "Orchard Organics", 
+                "BRF": "Big Red Farm",
+                "TS" : "Isles"
+        }
+
+        # alternative spellings included just in case the users inputs data wrong
+    speciesDict = {
+        "ChTom" : "cherry tomato",
+        "CherryTom": "cherry tomato",
+        "StTom" : "standard tomato",
+        "StdTom" : "standard tomato",
+        "chard" : "chard",
+        "chrd" : "chard", 
+        "Chard" : "chard", 
+        "Zuke" : "zucchini",
+        "zucke" : "zucchini",
+        "zuc" : "zucchini",
+    }
     print("Starting Program\n")
     try:
         #   toName = input("Enter the name of the CSV file you will be expanding : ")
         # fromName = input("Enter the name of the CSV file from which you will draw the data : ")
         toName = sys.argv[1]
+        print("Appending to ",toName)
         fromName = sys.argv[2]
+        print("from ", fromName)
         
     except:
         print("Invalid Input. Exiting...")
@@ -20,6 +46,37 @@ def main():
     # compare keys to make sure you can append correctly
     with open  (toName, newline='') as to:
         with open(fromName, newline='') as f:
+
+            # get the title of the farm
+            location = None
+            species = None
+            tempInF = True
+            for line in f:
+                line = line.replace(",", " ").replace("\"", " ").split(" ")
+                
+                # line = d.strip(" ") # separate species and location
+                # print(line, type(line))
+                if "Location" in line:
+                    print(line)
+                    try:
+                        farmInList = farms[line[2]]
+                    except KeyError:
+                        print(line[2])
+                        sys.exit("Farm not found in list")
+                    location = line[2]
+                    try: 
+                        species = speciesDict[line[3]]
+                    except KeyError:
+                        print(line[3])
+                        sys.exit("Crop not found in list")
+                      
+
+                if "°C" in line:
+                    tempInF = False
+                    sys.exit("Temperature is in Celcius instead of Fahrenheit. Reconfigure the output in arable")
+
+
+
             toListCols = []
             reader = csv.reader(to)
             for row in reader:   
@@ -33,14 +90,14 @@ def main():
                     fromListCols = row
                     break
 
-            if __debug__:
-                print("from", fromListCols)
+            # if __debug__:
+            #     print("from", fromListCols)
 
             
             fromValueToToIndex = defaultdict(lambda a: None)
             fromValueToToIndex    = {
                 "farm" : 0,
-                # "crop" : 1
+                "crop" : 1,
                 "local_device_time" : 2, 
                 # "julian date" : 3,
                 "device" : 4, 
@@ -48,11 +105,14 @@ def main():
                 "NDVI" : 11,
                 "ETc" :12,
                 "SWdw": 13,
-                "max_temp": 14,
-                "mean_temp": 15,
-                "min_temp": 16,
+                "cmax_temp": 14,  # celcius
+                "cmean_temp": 15, # celcius
+                "cmin_temp": 16, # celcius
                 "precip": 17,
-                }
+                "max_temp": 18,  # f
+                "mean_temp": 19, # f
+                "min_temp": 20, # f,
+                }                
 
 
             fromFromIndexToValue = defaultdict(lambda a: None)
@@ -76,7 +136,7 @@ def main():
                 16:  "SWdw" ,
                 17:  "max_temp",      
                 18:  "mean_temp" ,
-                19:   "min_temp",
+                19:  "min_temp",
                 20:  None,
                 21:  None,  
                 22: None,
@@ -87,24 +147,63 @@ def main():
             
             if __debug__:
                 print("\nENTERING READER APPEND LOOP\n")
+
+                ### append data into array ########
+        with open(fromName, newline='') as f:
             reader = csv.reader(f)
-            for row in reader:
-                print("\nRow in ", fromName, " : ", row)
+            for j in reader:
+                print("1")
                 t = 0
-                listToAppend = [None] * len(toListCols)
-                for item in row:
+                listToAppend = [None] * 25
+                for item in j:
                     indexToValue = fromFromIndexToValue.get(t, None)
                     if indexToValue != None:
                         valueToNewIndex = fromValueToToIndex.get(indexToValue, None)
                         listToAppend[valueToNewIndex] = item
                     else:
                         pass
-
                     t += 1
-                print("Appending")
+                print("Appending row ", j)
+
+                if __debug__:
+                    j = 0
+                    for i in listToAppend:
+                        print(j, i)
+                        j+=1
+
+                ########## clean up data ##########
+
                 # calc julian date
                 if listToAppend[2] != None:
+                    if __debug__:
+                        print("Julian date = ", csvOperations.calcJulian(listToAppend[2]))
                     listToAppend[3] = csvOperations.calcJulian(listToAppend[2])
+                
+                # calc celcius temps from f
+                maxTInCIndex= fromValueToToIndex["cmax_temp"]
+                minTInCIndex = fromValueToToIndex["cmin_temp"]
+                for i in range(maxTInCIndex, minTInCIndex + 1 ):
+                    # print(i, listToAppend[i])
+                    # if listToAppend[i] != None:
+                        # if __debug__:
+                        #     print("F value", plantFormulas.fToC(listToAppend[i]))
+                    listToAppend[i] = plantFormulas.fToC(listToAppend[i + 4])
+
+                # if __debug__:
+                #     print(listToAppend)
+                #     if listToAppend[20] != None and listToAppend[17] == None:
+                #         if listToAppend[17] != '':
+                #             sys.exit("error invalid temp C")
+
+                date = listToAppend[fromValueToToIndex["local_device_time"]]
+                if date != 0 or date != None:
+                    listToAppend[fromValueToToIndex["local_device_time"]] = csvOperations.changeTimeFormat(date)
+
+                listToAppend[fromValueToToIndex["farm"]] = location
+                listToAppend[fromValueToToIndex["crop"]] = species
+
+                print("Appending list : ", listToAppend, "\n")
+                        
                 csvOperations.append_list_as_row(toName, listToAppend)
                                 
 
