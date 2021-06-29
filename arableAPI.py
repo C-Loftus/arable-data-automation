211a5aeaebadb0809bfb9ba8bab907943575152f
colton <cloftus@princeton.edu>
2021-06-28 20:02:41 -0400

    Progress on API calls

--- arableAPI.py
+++ arableAPI.py
@@ -1,46 +1,77 @@
 import dotenv
-import base64 as bs
-from datetime import date
 from dotenv.main import load_dotenv
 import os, requests
 import arablepy
-import pandas as pd
 from requests.sessions import session
 import serialNums
 import sys
+import json
+from base64 import b64encode
+
+# def main():
+    # client = arablepy.ArableClient()
+
+    # dotenv.load_dotenv()
+    # username = os.environ.get("USERN")
+    # password = os.environ.get("PASSW")
+    # if username == None or password == None:
+    #     sys.exit("No password / username")
+
+    # client.connect(email=username, password=password)
+    # client.schema('daily', df=True)
+
+    # # tomato = serialNums.private.data()["AA ChTom"] 
+    # # same as above, just hardcoded to show example
+    # tomato = "C003649"
+
+    # values = client.data(
+    #                 'daily',
+    #                 devices= [tomato],
+    #                 # order='time',
+    #                 start_time="2021-06-03T00:00:00Z",
+    #                 end_time="2021-06-17",
+    #                 df = True)
 
 
-client = arablepy.ArableClient()
+    # print(values)
+    # return values
+    # ^^ always returns/prints an empty dataframe for some reason
 
-dotenv.load_dotenv()
-username = os.environ.get("USERN")
-password = os.environ.get("PASSW")
-if username == None or password == None:
-    sys.exit("No password / username")
+def callAPI():
+    dotenv.load_dotenv()
+    username = os.environ.get("USERN")
+    password = os.environ.get("PASSW")
 
-client.connect(email=username, password=password)
-client.schema('daily', df=True)
+    token = b64encode("{0}:{1}".format(username, password).encode('utf-8')).decode('utf-8')
 
-tomato = serialNums.private.data()["AA ChTom"]
-# print(tomato)
-tomato = "C003649"
-
-values = client.data(
-                'daily',
-                devices= [tomato],
-                # order='time',
-                start_time="2021-06-03T00:00:00Z",
-                end_time="2021-06-17",
-                df = True)
+    r = requests.get("https://api.arable.cloud/api/v2/devices", headers={"Authorization": "Basic " + token})
 
 
-print(values)
-# ^^ always returns an empty dataframe for some reason
+    # alternative way to get the token
+        # curl -X POST \
+        #  https://api.arable.cloud/api/v2/auth/token \
+        #  -H 'Content-Type: application/json' \
+        #  -d '{"email": "<email address>", "password": "<plaintext password>"}'
+        # 
+    # r = requests.post("https://api.arable.cloud/api/v2/auth/token", headers={"Content-Type" : "application/json"},
+    #  data={"email": username, "password": password})
 
 
+  
+    # Initialize JSON data
 
+    
+    # Create Python object from JSON string data
+    obj = r.json()
+    
+    # Pretty Print JSON
+    json_formatted_str = json.dumps(obj, indent=4)
+    print(json_formatted_str)
 
+    print(r.encoding)
 
+if __name__ == "__main__":
+    callAPI()
 
 
 
--- drive.py
+++ drive.py
@@ -1,8 +1,66 @@
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 import serialNums
+import json
 
 def uploadFile(fileToUpload):
+    gauth = GoogleAuth()
+    # Try to load saved client credentials
+    gauth.LoadCredentialsFile("conf/mycreds.txt")
+    if gauth.credentials is None:
+        # Authenticate if they're not there
+        gauth.LocalWebserverAuth()
+    elif gauth.access_token_expired:
+        # Refresh them if expired
+        gauth.Refresh()
+    else:
+        # Initialize the saved creds
+        gauth.Authorize()
+    # Save the current credentials to a file
+    gauth.SaveCredentialsFile("conf/mycreds.txt")
+
+    drive = GoogleDrive(gauth)
+
+    folderName = serialNums.private.returnFolderName() # set the folder name.
+
+
+    folderId = None
+    folders = drive.ListFile(
+        {'q': "title='" + folderName + "' and mimeType='application/vnd.google-apps.folder' and trashed=false"}).GetList()
+    for folder in folders:
+        if folder['title'] == folderName:
+            folderId = folder['id']
+
+        
+
+
+    allFiles = drive.ListFile({'q': "'" + folderId + "' in parents and trashed=false"}).GetList()
+    # obj = allFiles
+    # # Pretty Print JSON
+    # json_formatted_str = json.dumps(obj, indent=4)
+    # print(json_formatted_str)
+
+    for file in allFiles:
+        file2 = drive.CreateFile({'id': file['id']})
+        print(file2['title'])
+        file2['parents'] = [{"kind": "drive#parentReference", "id": "1TYE9cgIGU8hbaygdGL2sCYAi-YTTadOz"}]
+        try:
+            file2.Upload()
+        except:
+            pass
+
+
+    # uploading is correct
+    folders = drive.ListFile(
+        {'q': "title='" + folderName + "' and mimeType='application/vnd.google-apps.folder' and trashed=false"}).GetList()
+    for folder in folders:
+        if folder['title'] == folderName:
+            file2 = drive.CreateFile({'parents': [{'id': folder['id']}]})
+            file2.SetContentFile(fileToUpload)
+            file2.Upload()
+
+
+def downloadFile():
     gauth = GoogleAuth()
     # Try to load saved client credentials
     gauth.LoadCredentialsFile("conf/mycreds.txt")
@@ -41,9 +99,6 @@ def uploadFile(fileToUpload):
             file2 = drive.CreateFile({'parents': [{'id': folder['id']}]})
             file2.SetContentFile(fileToUpload)
             file2.Upload()
-
-
-
 
 if __name__ == "__main__":
     testInput = input("Give your file name : ")
